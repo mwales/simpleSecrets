bc42004bd13dfc75c72dae51830e7a5c3ce8b0d8
Michael Wales <mwales3@gmail.com>
2019-01-22 23:45:30 -0500

    Moved test applications to their own directory

--- src/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-gcc md5.c sha1.c sha2.c hash_test.c -o hash_test
-
-gcc hmac_sha1.c sha1.c hmac_test.c -o hmac_test
-
-gcc pbkdf2.c hmac_sha1.c sha1.c pbkdf_test.c -o pbkdf_test
-
---
+++ test/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+gcc -I../src ../src/md5.c ../src/sha1.c ../src/sha2.c hash_test.c -o hash_test
+
+gcc -I../src ../src/hmac_sha1.c ../src/sha1.c hmac_test.c -o hmac_test
+
+gcc -I../src ../src/pbkdf2.c ../src/hmac_sha1.c ../src/sha1.c pbkdf_test.c -o pbkdf_test
+
